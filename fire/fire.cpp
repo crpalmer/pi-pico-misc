cd8017ac69138639495e7453e46e6967653cf8dc
Christopher R. Palmer <crpalmer@gmail.com>
2022-09-04 06:58:14 -0400

    fire: add in a red color too

--- fire/fire.cpp
+++ fire/fire.cpp
@@ -14,8 +14,8 @@ typedef struct {
 } color_t;
 
 static color_t orange = { 226, 121,  35 };
-static color_t purple = { 158,   8, 148 };
-//static color_t green  = {  74, 150,  12 };
+static color_t purple = { 131,  56, 154 };
+static color_t red    = { 203,  89,  67 };
 
 #define SLEEP_LOW 10
 #define SLEEP_HIGH 100
@@ -27,7 +27,8 @@ static critical_section cs;
 static NeoPixelPico *fire_neo;
 static bool is_paused = false;
 static struct { int low, high; } flicker = { 0, 55 };
-static int purple_pct = 10;
+static int purple_pct = 3;
+static int red_pct = 12;
 
 static void set_is_paused(bool new_is_paused)
 {
@@ -46,11 +47,11 @@ static void resume_fire()
     set_is_paused(false);
 }
 
-static void flicker_led(NeoPixelPico *neo, int led, color_t *c)
+static void flicker_led(NeoPixelPico *neo, int led, color_t c)
 {
-    int r = c->r - random_number_in_range(flicker.low, flicker.high);
-    int g = c->g - random_number_in_range(flicker.low, flicker.high);
-    int b = c->b - random_number_in_range(flicker.low, flicker.high);
+    int r = c.r - random_number_in_range(flicker.low, flicker.high);
+    int g = c.g - random_number_in_range(flicker.low, flicker.high);
+    int b = c.b - random_number_in_range(flicker.low, flicker.high);
 
     if (r < 0) r = 0;
     if (g < 0) g = 0;
@@ -62,11 +63,14 @@ static void flicker_led(NeoPixelPico *neo, int led, color_t *c)
 static void flicker_all(NeoPixelPico *neo)
 {
     for (int i = 0; i < N_LEDS; i++) {
-	if (random_number_in_range(0, 100) < purple_pct) {
-	    flicker_led(neo, i, &purple);
-	} else {
-	    flicker_led(neo, i, &orange);
-	}
+	int pct = random_number_in_range(0, 99);
+	color_t c;
+
+	if (pct < purple_pct) c = purple;
+	else if (pct < red_pct + purple_pct) c = red;
+	else c = orange;
+
+	flicker_led(neo, i, c);
     }
 }
 
@@ -101,13 +105,24 @@ main(int argc, char **argv)
 
 	while (! tud_cdc_connected()) ms_sleep(100);
 
-	pico_readline(line, sizeof(line));
+	pico_readline_echo(line, sizeof(line), true);
 	int space = 0;
 	while (line[space] && line[space] != ' ') space++;
 
 	if (strcmp(line, "bootsel") == 0) {
             printf("Rebooting into bootloader mode...\n");
             reset_usb_boot(1<<PICO_DEFAULT_LED_PIN, 0);
+	} else if (strcmp(line, "dump") == 0) {
+	    printf("flicker       %3d %3d\n", flicker.low, flicker.high);
+	    printf("orange        %3d %3d %3d\n", orange.r, orange.g, orange.b);
+	    printf("purple        %3d %3d %3d\n", purple.r, purple.g, purple.b);
+	    printf("red           %3d %3d %3d\n", red.r, red.g, red.b);
+	    printf("percentages   %3d %3d %3d\n", 100-purple_pct-red_pct, purple_pct, red_pct);
+	} else if (strncmp(line, "flicker ", space+1) == 0) {
+	    pause_fire();
+	    sscanf(&line[space], "%d %d\n", &flicker.low, &flicker.high);
+	    resume_fire();
+	    printf("flicker %3d %3d\n", flicker.low, flicker.high);
 	} else if (strncmp(line, "orange ", space+1) == 0) {
 	    pause_fire();
 	    sscanf(&line[space], "%d %d %d\n", &orange.r, &orange.g, &orange.b);
@@ -123,17 +138,24 @@ main(int argc, char **argv)
 	    purple_pct = atoi(&line[space]);
 	    resume_fire();
 	    printf("purple_pct %d\n", purple_pct);
-	} else if (strncmp(line, "flicker ", space+1) == 0) {
+	} else if (strncmp(line, "red ", space+1) == 0) {
 	    pause_fire();
-	    sscanf(&line[space], "%d %d\n", &flicker.low, &flicker.high);
+	    sscanf(&line[space], "%d %d %d\n", &red.r, &red.g, &red.b);
 	    resume_fire();
-	    printf("flicker %3d %3d\n", flicker.low, flicker.high);
+	    printf("red color %3d %3d %3d\n", red.r, red.g, red.b);
+	} else if (strncmp(line, "red_pct ", space+1) == 0) {
+	    pause_fire();
+	    red_pct = atoi(&line[space]);
+	    resume_fire();
+	    printf("red_pct %d\n", red_pct);
 	} else {
 	    printf("bootsel - reboot for flashing\n");
 	    printf("flicker low high - set range of flicker\n");
-	    printf("orange r g b - set base color\n");
-	    printf("purple r g b - set base color\n");
+	    printf("orange r g b - set orange color\n");
+	    printf("purple r g b - set purple color\n");
 	    printf("purple_pct pct - set amount of purple flickers\n");
+	    printf("red r g b - set red color\n");
+	    printf("red_pct pct - set amount of red flickers\n");
 	}
     }
 }
